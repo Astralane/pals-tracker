87053263e7d866b1b468549be741711a3b78a050
Sjain-dir <saksham.jain.code@gmail.com>
2025-08-05 14:50:58 +0530

    changed from tpu to gossip ip

--- src/palidator_cache.rs
+++ src/palidator_cache.rs
@@ -95,8 +95,8 @@ impl PalidatorCache {
         node: &RpcContactInfo,
     ) -> Option<String> {
         let key = node.pubkey.clone();
-        let to_1 = node.tpu_quic.map(|s| SocketAddr::new(s.ip(), PAL_PORT_1))?;
-        let to_2 = node.tpu_quic.map(|s| SocketAddr::new(s.ip(), PAL_PORT_2))?;
+        let to_1 = node.gossip.map(|s| SocketAddr::new(s.ip(), PAL_PORT_1))?;
+        let to_2 = node.gossip.map(|s| SocketAddr::new(s.ip(), PAL_PORT_2))?;
         if let Ok(connecting) = endpoint.connect(to_1, "connect") {
             if connecting.await.is_ok() {
                 return Some(key);
